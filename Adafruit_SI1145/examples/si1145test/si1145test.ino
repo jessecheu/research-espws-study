f08093fddcfb12db94a1c49dd514cf88ff950ccc
Jesse Cheung <jesse.cheung67@gmail.com>
2024-08-12 16:14:59 -0700

    first, second iteration

--- Adafruit_SI1145/examples/si1145test/si1145test.ino
+++ Adafruit_SI1145/examples/si1145test/si1145test.ino
@@ -1,3 +1,23 @@
+/*************************************************** 
+  This is a library for the Si1145 UV/IR/Visible Light Sensor
+
+  Designed specifically to work with the Si1145 sensor in the
+  adafruit shop
+  ----> https://www.adafruit.com/products/1777
+
+  These sensors use I2C to communicate, 2 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <Wire.h>
+#include "Adafruit_SI1145.h"
+
 /**
  * Adapted from BLE Client Example
  */
@@ -76,8 +96,23 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
   }
 };
 
+Adafruit_SI1145 uv = Adafruit_SI1145();
+
 void setup() {
-  BLEDevice::init("Your number here");
+  Serial.begin(9600);
+  
+  Serial.println("Adafruit SI1145 test");
+  
+  if (! uv.begin()) {
+    Serial.println("Didn't find Si1145");
+    while (1);
+  }
+
+  Serial.println("OK!");
+
+  //END LIGHT SENSOR
+
+    BLEDevice::init("Your number here");
   Serial.begin(115200);
 
   // Turn LED off on esp for sleep
@@ -96,8 +131,27 @@ void setup() {
 }
 
 void loop() {
+  Serial.println("===================");
+  Serial.print("Vis: "); Serial.println(uv.readVisible());
+  Serial.print("IR: "); Serial.println(uv.readIR());
+  
+  // Uncomment if you have an IR LED attached to LED pin!
+  //Serial.print("Prox: "); Serial.println(uv.readProx());
 
-  if (doConnect == true) {
+  float UVindex = uv.readUV();
+  // the index is multiplied by 100 so to get the
+  // integer index, divide by 100!
+  UVindex /= 100.0;  
+  Serial.print("UV: ");  Serial.println(UVindex);
+
+  delay(1000);
+
+  //END LIGHT CODE
+
+  Serial.println("Attempting to data to the server...");
+  //START BLE CODE
+
+    if (doConnect == true) {
     connectToServer();
   }
 
@@ -106,10 +160,11 @@ void loop() {
 
     // Format readings into: "Name,sensor,sensor_reading,sensor1,sensor1_readingg"
     // Example
-    String sensor_readings = "Berk,temp,72.1";
+    Serial.println("Sending data to the server...");
+    String sensor_readings = "Berk, example, 1.0";
     // Can use .concat() to append values to string
     sensor_readings.concat(",light,");
-    sensor_readings.concat(0.78);
+    sensor_readings.concat(UVindex);
     Serial.println(sensor_readings);
 
     std::string str = sensor_readings.c_str();
@@ -120,4 +175,6 @@ void loop() {
     delay(5000);
     deepSleep();
   }
+
+  //END BLE CODE
 }
